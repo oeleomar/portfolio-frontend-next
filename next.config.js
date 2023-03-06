4b07fc8b9a50da490ac29465d5ed6842106f8ad9
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-03-06 17:22:31 -0400

    Bug fixing

--- next.config.js
+++ next.config.js
@@ -10,7 +10,6 @@ const nextConfig = {
       { protocol: "https", hostname: "res.cloudinary.com", port: "" },
     ],
   },
-  assetPrefix: "https://res.cloudinary.com",
 };
 
 module.exports = nextConfig;
